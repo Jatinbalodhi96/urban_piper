cf044a6570951922f202c069973dc646d341b533
Jatin Balodhi <jatin.balodhi@gramener.com>
2019-05-08 01:29:16 +0530

    WIP: added models and templates

--- up_web/views.py
+++ up_web/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Task
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    tasks = Task.objects.all()
+    context = {'tasks': tasks}
+    return render(request, 'index.html', context)
+
+def createTask(request):
+    return HttpResponse(200)
--- urban_piper/settings.py
+++ urban_piper/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'up_web.apps.UpWebConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
